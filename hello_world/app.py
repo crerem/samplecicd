606fb01552206b7cbcc02c31a8aa538e025c9679
Remus Cretu <crerem@gmail.com>
2023-09-12 15:20:15 +0300

    final

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "one final push",
+            "message": "one final push - acum fa articolul :) ",
             # "location": ip.text.replace("\n", "")
         }),
     }
